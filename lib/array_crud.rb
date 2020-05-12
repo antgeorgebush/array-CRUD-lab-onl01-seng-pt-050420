0fd4312b787d899435b96c65405e9ec2c966ec7f
Anthony George <ageorge530@gmail.com>
2020-05-12 06:46:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["wow", "I", "am", "really", "learning"]
-  element < "arrays!"
+  array = ["Alex", "Joe", "Martin", "Tony"]
+  element = "Ace"
 end
   
 def add_element_to_start_of_array(array, element)
